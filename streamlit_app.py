08b32821523bd3681759d3712dc4fe183b85d526
Eduardo Nakatani <55983395+ednakatani@users.noreply.github.com>
2022-09-18 22:45:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ Os algoritmos são experimentais, não garantindo nenhuma precisão do resultado
 """
 
 
-with st.echo(code_location='below'):
+with st.echo():
     area = st.number_input('Área Construida (m²)')
     quartos = st.number_input('Quartos', step=1)
     banheiros = st.number_input('Banheiros', step=1)

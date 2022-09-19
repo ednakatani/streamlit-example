ffd2ef6351d278eb26b3862610c3dfb46c84584b
Eduardo Nakatani <55983395+ednakatani@users.noreply.github.com>
2022-09-18 23:20:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-from sklearn.externals import joblib
+import joblib
 from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 
 from warnings import simplefilter

48f8781001aacb1142fa26294c33973dfe3bc0bd
Eduardo Nakatani <55983395+ednakatani@users.noreply.github.com>
2022-09-18 22:32:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,23 +16,9 @@ Os algoritmos são experimentais, não garantindo nenhuma precisão do resultado
 
 
 with st.echo(code_location='below'):
-    area = st.text_input('Área')
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    area = st.number_input('Área Construida (m²)')
+    quartos = st.number_input('Quartos')
+    banheiros = st.number_input('Banheiros')
+    vagas = st.number_imput('Vagas')
+    
+    
